3bb3917a888210752c66eb68bdc160891cea8d4f
Aram <AramSabriMousa@student.han.nl>
2022-01-24 12:23:35 +0100

    Add tables and data

--- sqlite3-with-java/src/main/java/db/persistence/Database.java
+++ sqlite3-with-java/src/main/java/db/persistence/Database.java
@@ -1,21 +1,35 @@
 package db.persistence;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+
+import static java.lang.System.*;
 
 public class Database {
 
     public static void getConnection(){
         try (
                 Connection conn = DriverManager.getConnection("jdbc:sqlite:sqlite3-with-java\\src\\main\\resources\\myfirstdbinjava.db");
-                Statement stm = conn.createStatement();
-                ) {
-            System.out.println("Connected to DB!");
-            stm.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
+                Statement stm = conn.createStatement()
+        ) {
+            out.println("Connected to DB!");
+            String sqlCreateTable = "CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)";
+            stm.execute(sqlCreateTable);
+            String sqlInsertData = "INSERT INTO contacts (name, phone, email) VALUES ('Jan', 1111, 'J@gmail.nl')," +
+                    "('Peter', 1111, 'p@gmail.nl')," +
+                    "('Sara', 1111, 's@gmail.nl')," +
+                    "('Sanne', 1111, 'sn@gmail.nl')";
+            //stm.execute(sqlInsertData);
+            //stm.execute("update contacts set phone = '4444' where name = 'Sanne'");
+
+            // Get Data
+            ResultSet results = stm.executeQuery("SELECT * FROM contacts");
+            while (results.next()) {
+                out.print(results.getString("name") + " -- ");
+                out.print(results.getInt("phone") + " -- ");
+                out.println(results.getString("email"));
+            }
         } catch (SQLException e) {
-            System.out.println("Ops! Something went wrong " + e.getMessage());
+            out.println("Ops! Something went wrong " + e.getMessage());
         }
     }
 
